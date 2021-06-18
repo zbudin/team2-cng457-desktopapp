2f9195e9599613431f675fc2d7565f740e0582d3
ZekicanBudin <44768735+ZekicanBudin@users.noreply.github.com>
2021-06-18 23:27:12 +0300

    Initial commit

---
+++ src/desktopapp/Controller.java
@@ -0,0 +1,4 @@
+package desktopapp;
+
+public class Controller {
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module team2.cng457.desktopapp {
+
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+
+    opens desktopapp;
+}
