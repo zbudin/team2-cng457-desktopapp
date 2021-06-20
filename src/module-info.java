be97947aef8f04d09eb155a0c4b586f6bb6fe6d0
Akın Gülpınar <akin.gulpinar@softtech.com.tr>
2021-06-20 17:20:56 +0300

    module info update

--- src/module-info.java
+++ src/module-info.java
@@ -5,5 +5,13 @@ module team2.cng457.desktopapp {
     requires javafx.fxml;
     requires javafx.base;
 
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+
+    opens desktopapp.Controllers;
+    opens desktopapp.Models;
+    opens desktopapp.Views;
+
     opens desktopapp;
 }
